ffc4633599c581364b9a64c04a6b750da320e6ba
Markus Olsen <molsen13@gmail.com>
2016-03-29 22:29:22 -0500

    draw ascii art to screen

--- main.go
+++ main.go
@@ -18,9 +18,12 @@ func main() {
 		return
 	}
 
-	if args[0] == "fonts" {
+	switch args[0] {
+	case "fonts":
 		fmt.Printf("%v", fontList())
 	}
+
+	fmt.Println(draw(args[0]))
 }
 
 func fontList() string {
@@ -39,4 +42,22 @@ func fontList() string {
 
 	s := string(body) + "\n"
 	return s
+}
+
+func draw(s string) string {
+	url := fmt.Sprintf("%s/make?text=%s", aURL, s)
+
+	resp, err := http.Get(url)
+	if err != nil {
+		log.Fatal(err)
+	}
+	defer resp.Body.Close()
+
+	body, err := ioutil.ReadAll(resp.Body)
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	a := string(body)
+	return a
 }
